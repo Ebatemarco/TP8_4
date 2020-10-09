40ea4753e2300df29a64cfe346047eea15dd1fac
ezediazhuzman <72627168+ezediazhuzman@users.noreply.github.com>
2020-10-09 16:30:47 -0300

    Add files via upload
    Main prueba :)

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Grupo 3
+ *
+ * Created on October 9, 2020, 4:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
